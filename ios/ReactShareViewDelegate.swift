71f4b2c8cdc73743d16b29c8918c7dea27fc2ed1
Martin <8627696+MartinInAction@users.noreply.github.com>
2022-05-23 12:43:24 +0200

    Update ReactShareViewDelegate.swift

--- ios/ReactShareViewDelegate.swift
+++ ios/ReactShareViewDelegate.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Gustavo Parreira on 29/07/2020.
 //
-import Firebase
 
 public protocol ReactShareViewDelegate {
     func loadExtensionContext() -> NSExtensionContext
@@ -13,21 +12,3 @@ public protocol ReactShareViewDelegate {
 
     func continueInApp(with items: [NSExtensionItem], and extraData: [String:Any]?)
 }
-
-func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-    setupFirebase()
-    return true
-}
-
-
-func setupFirebase () {
-    let env = ReactNativeConfig.env(for: "ENVIRONMENT")
-    var firebasePlistFileName = "GoogleService-Info"
-    if (env == "Dev") {
-      firebasePlistFileName = "GoogleService-Info-Dev"
-    }
-    let filePath = Bundle.main.path(forResource: firebasePlistFileName, ofType: "plist")
-    let fileopts = FirebaseOptions.init(contentsOfFile: filePath!)!
-
-    FirebaseApp.configure(options: fileopts)
-  }
